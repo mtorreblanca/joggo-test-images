40888526f2e80cffe6ba172f3009cbdb7a62e0c9
mtorreblanca <m_torreblanca@outlook.com>
2021-03-18 19:24:23 -0600

    Mock photos added, installation will be only through fetch api

--- src/components/AppDragDropDemo.js
+++ src/components/AppDragDropDemo.js
@@ -1,7 +1,23 @@
+// import { createApi } from 'unsplash-js';
+
+// const unsplash = new createApi({
+//     accessKey: process.env.REACT_APP_UNSPLASH_ACCESS,
+// });
+import photos from './photos.json';
+
 const DragAndDropContainer = () => {
+    console.log(photos)
     return (
         <div className="flex">
-            <div className="w-1/2">splash</div>
+            <div className="w-1/2 grid grid-flow-col grid-cols-3 grid-rows-3 gap-4 m-4">
+                {photos.map((photo) =>
+                    <img key={photo.id}
+                        className="h-20 w-20 mx-auto"
+                        src={photo.urls.full}
+                        alt={photo.alt_description}
+                    />)
+                }
+            </div>
             <div className="flex-col h-screen">
                 <div className="h-1/2">accepted</div>
                 <div>neglected</div>
