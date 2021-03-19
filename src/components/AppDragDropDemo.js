6e89945f475dd625abb0f0cd1edec2d249873b3d
mtorreblanca <m_torreblanca@outlook.com>
2021-03-18 21:50:12 -0600

    UI changes: background-color and icons

--- src/components/AppDragDropDemo.js
+++ src/components/AppDragDropDemo.js
@@ -1,8 +1,11 @@
 import { useState, useEffect } from 'react';
 import getPhotos from '../services/getPhotos';
+import photos from './photos.json';
+import neglectedIcon from '../assets/icons/neglected.svg';
+import acceptedIcon from '../assets/icons/accept.svg';
 
 const DragAndDropContainer = () => {
-    const [initial, setInitial] = useState([]);
+    const [initial, setInitial] = useState(photos);
     const [accepted, setAccepted] = useState([]);
     const [neglected, setNeglected] = useState([]);
 
@@ -73,8 +76,8 @@ const DragAndDropContainer = () => {
     }
 
     return (
-        <div className="flex h-screen overflow-hidden">
-            <div className="w-1/2 h-100 overflow-hidden grid grid-flow-col grid-cols-3 grid-rows-3 gap-2 m-1"
+        <div className="flex h-screen overflow-hidden bg-gray-50">
+            <div className="w-1/2 h-100 overflow-hidden grid grid-flow-col grid-cols-3 grid-rows-3 gap-2 m-1 "
                 onDragOver={(e) => onDragOver(e)}
                 onDrop={(e) => { onDrop(e, "initial") }}
             >
@@ -90,28 +93,36 @@ const DragAndDropContainer = () => {
                 }
             </div>
             <div className="flex-col w-1/2 h-100">
-                <div className="h-1/2 w-full grid grid-flow-col grid-cols-3 grid-rows-3 gap-1"
+                <div className="h-1/2 w-full grid grid-flow-col grid-cols-3 grid-rows-3 gap-1 bg-green-50 relative"
                     onDragOver={(e) => onDragOver(e)}
                     onDrop={(e) => { onDrop(e, "accepted") }}>
+                    <div className="absolute right-2 top-2">
+                        <img src={acceptedIcon}
+                            alt="neglected icon" />
+                    </div>
                     {accepted.map((image, index) =>
                         <img key={image.id}
                             onDragStart={(e) => onDragStart(e, index, 'accepted')}
                             draggable
-                            className="h-20 w-20 m-auto"
+                            className="h-20 w-20 m-auto mt-2"
                             src={image.urls.full}
                             alt={image.alt_description}
                         />)
                     }
                 </div>
-                <div className=" w-full h-1/2 grid grid-flow-col grid-cols-3 grid-rows-3 gap-1
-                    border border-l-1"
+                <div className="w-full h-1/2 grid grid-flow-col grid-cols-3 grid-rows-3 gap-1 border-t-2
+                    bg-red-50 relative"
                     onDragOver={(e) => onDragOver(e)}
                     onDrop={(e) => { onDrop(e, "neglected") }}>
+                    <div className="absolute right-2 top-2">
+                        <img src={neglectedIcon}
+                            alt="neglected icon" />
+                    </div>
                     {neglected.map((image, index) =>
                         <img key={image.id}
                             onDragStart={(e) => onDragStart(e, index, 'neglected')}
                             draggable
-                            className="h-20 w-20 m-auto"
+                            className="h-20 w-20 mx-auto mt-2"
                             src={image.urls.full}
                             alt={image.alt_description}
                         />)
